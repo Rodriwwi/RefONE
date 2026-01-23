b4a60b2f888b5f4e9a9482a5872342dc1cee80f3
Rodriwwi <rodrigo.vicentev05@gmail.com>
2026-01-23 13:47:02 +0100

    RefONE 1.0

---
+++ RefONE Watch App/RefONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefONEApp.swift
+//  RefONE Watch App
+//
+//  Created by Rodri on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RefONE_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch Watch App/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch Watch App
+//
+//  Created by Rodri on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
